7f5c517e2ec2be7220317f85c1bf356593323fab
Champy8 <40555033+Champy8@users.noreply.github.com>
2018-06-26 15:10:04 +0700

    Update headbot.php

--- headbot.php
+++ headbot.php
@@ -1,6 +1,6 @@
 <?php
 
-$access_token = 'vIqVV9lNX5yNkf7r4nm+FFAesNeaypSuYC/OOW9LOiRptDrt0/ELtOJekuhmabamyn5ssrtDODisO/XE2wvauE7MTr1C0xIY84aHbRZRQDRtEojxs7UtkvssK7Y4eS4Xj/r+krB7u9ueoZVAjmOvMwdB04t89/1O/w1cDnyilFU=';
+$access_token = '5ef2CHbT4UGuCFuO8ps54DGpUmhH56jfb2Fl6Vxe6bDNWEYmjiZvIgvBGOB/kEUzev46GXCyPSfLcYrzDKCMU0cJxYaGfdMNotJQkcZsfMW2WMrawkmAwazRygk1dOCaKtpSQJ+QxxnTR+2agkgunQdB04t89/1O/w1cDnyilFU=';
 
 $baseUrl = "xxxxx";
 
